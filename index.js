8a513f9b60cf3fa9f06623effb5c3b9ed5d8a76c
fkskgit <fshaikh0217@conestogac.on.ca>
2023-09-26 18:18:45 -0400

    Farheen work

--- index.js
+++ index.js
@@ -1,160 +1,191 @@
 // import dependencies you will use
 const express = require('express');
 const path = require('path');
-//const bodyParser = require('body-parser'); // not required for Express 4.16 onwards as bodyParser is now included with Express
-// set up expess validator
-const { check, validationResult } = require('express-validator'); //destructuring an object
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
-// Requiring the session package
 const session = require('express-session');
+const { check, validationResult } = require('express-validator');
+const nameRegex = /^[a-zA-Z0-9]{1,}\s[a-zA-Z0-9]{1,}$/;
+
 mongoose.connect('mongodb://127.0.0.1:27017/NexaConnect');
-// defining collection:
+
+// Define MongoDB models
 const Blog = mongoose.model('Blog', {
     Name: String,
     Email: String,
     Description: String,
     ImagePath: String,
 });
-// create a model for User
+
 const User = mongoose.model('User', {
     username: String,
-    password: String
-})
-// set up variables to use packages
-var myApp = express();
-// myApp.use(bodyParser.urlencoded({extended:false})); // old way before Express 4.16
-myApp.use(express.urlencoded({ extended: false })); // new way after Express 4.16
+    password: String,
+});
 
-// setting up the session
+const Payment = mongoose.model('Payment', {
+    userId: String,
+    amount: Number,
+    cardNumber: String,
+    cardHolderName: String,
+    expirationDate: String,
+    postalCode: String,
+    // Add other fields as needed
+});
+
+const myApp = express();
+
+myApp.use(express.urlencoded({ extended: false }));
 myApp.use(session({
     secret: 'randomsecretcode',
     resave: false,
-    saveUninitialized: true
-}))
-// set path to public folders and view folders
+    saveUninitialized: true,
+}));
 
 myApp.set('views', path.join(__dirname, 'views'));
-//use public folder for CSS etc.
 myApp.use(express.static(__dirname + '/public'));
 myApp.set('view engine', 'ejs');
-// using fileUpload
 myApp.use(fileUpload());
-
-var nameRegex = /^[a-zA-Z0-9]{1,}\s[a-zA-Z0-9]{1,}$/;
 const filePath = 'public/images/';
-// set up different routes (pages) of the website
-// Ignore favicon requests
+
 myApp.get('/favicon.ico', (req, res) => res.status(204));
 
-// render the home page
-myApp.get('/', function (req, res) {
-    res.render('home'); // will render views/home.ejs
+myApp.get('/', (req, res) => {
+    res.render('home');
 });
 
-// render the login page
-myApp.get('/login', function (req, res) {
-    res.render('login'); // will render views/login.ejs
+myApp.get('/login', (req, res) => {
+    res.render('login');
 });
-// render the signup page
-myApp.get('/signup', function (req, res) {
-    res.render('signup'); // will render views/login.ejs
+
+myApp.get('/signup', (req, res) => {
+    res.render('signup');
 });
-myApp.post('/signin', function (req, res) {
-    var pageData = {
+
+myApp.post('/signin', (req, res) => {
+    const pageData = {
         username: req.body.userName,
-        password: req.body.password
+        password: req.body.password,
     };
-    User.findOne(pageData).then((user) => {
-        req.session.username = user.username;
-        req.session.isLoggedIn = true;
-        res.render('home');
-    }).catch((err) => {
-        console.log(err);
-        res.render('login');
-    })
-})
+
+    User.findOne(pageData)
+        .then((user) => {
+            req.session.userId = user._id; // Store the user's ID in the session
+            res.render('home');
+        })
+        .catch((err) => {
+            console.error(err);
+            res.render('login');
+        });
+});
 
 myApp.post('/createaccount', [
     check('userName', 'please enter username').not().isEmpty(),
-    check('password', 'please enter password').not().isEmpty(),]
-    , function (req, res) {
-        const errors = validationResult(req);
-        if (errors.isEmpty()) {
-            var pageData = {
-                username: req.body.userName,
-                password: req.body.password
-            };
-            var userNew = new User(pageData);
-            userNew.save();
-            res.render('login');
-        }
+    check('password', 'please enter password').not().isEmpty(),
+], (req, res) => {
+    const errors = validationResult(req);
 
-    })
+    if (errors.isEmpty()) {
+        const pageData = {
+            username: req.body.userName,
+            password: req.body.password,
+        };
+
+        const userNew = new User(pageData);
+        userNew.save();
+        res.render('login');
+    }
+});
+
 myApp.post('/process', [
     check('Description', 'Please enter a description.').not().isEmpty(),
     check('Email', 'Please enter a valid email').isEmail(),
-    check('Name', 'Please enter firstname and lastname').matches(nameRegex)
-], function (req, res) {
-
-    // check for errors
+    check('Name', 'Please enter firstname and lastname').matches(nameRegex),
+], (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
-    if (!errors.isEmpty()) {
 
+    if (!errors.isEmpty()) {
         res.render('home', { er: errors.array() });
-    }
-    else {
-        //fetch all the form fields
-        var Name = req.body.Name; // the key here is from the name attribute not the id attribute
-        var Email = req.body.Email;
-        var Description = req.body.Description;
-        console.log(Name + "  " + Email + "  " + Description);
-        // create an object with the fetched data to send to the view
-        var Image = req.files.Image;
-        var ImageName = Image.name;
-        var ImageSavePath = filePath + ImageName;
-        Image.mv(ImageSavePath, function (err) {
-            if (err)
-                console.log(err);
-        })
-        var pageData = {
+    } else {
+        const Name = req.body.Name;
+        const Email = req.body.Email;
+        const Description = req.body.Description;
+
+        const Image = req.files.Image;
+        const ImageName = Image.name;
+        const ImageSavePath = filePath + ImageName;
+        Image.mv(ImageSavePath, (err) => {
+            if (err) console.error(err);
+        });
+
+        const pageData = {
             Name: Name,
             Email: Email,
             Description: Description,
             ImagePath: 'images/' + ImageName,
+        };
 
-        }
-        var blogNew = new Blog(pageData);
+        const blogNew = new Blog(pageData);
         blogNew.save();
-        // send the data to the view and render it
         res.render('blog', pageData);
     }
 });
 
-// returns all blogs
-myApp.get('/blogs', function (req, res) {
-    Blog.find({}).then((blogs) => {
-        res.render('blogview', { blogs: blogs });
-    }).catch((err) => {
-        console.log(err);
-    })
-})
+myApp.get('/blogs', (req, res) => {
+    Blog.find({})
+        .then((blogs) => {
+            res.render('blogview', { blogs: blogs });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
 
-// create a get request to retrieve blog by ID
-myApp.get('/:blogid', function (req, res) {
-    Blog.findOne({ _id: req.params.blogid }).then((blog) => {
-        // send the data to the view and render it
-        res.render('blogview', { blogs: [blog] });
-    }).catch((err) => {
-        console.log(err);
-    })
-})
+myApp.get('/payment', (req, res) => {
+    res.render('payment');
+});
 
-// start the server and listen at a port
-myApp.listen(8080);
+myApp.post('/processPayment', async (req, res) => {
+    const { amount, cardNumber, cardHolderName, expirationDate, postalCode } = req.body;
+    const userId = req.session.userId; // Retrieve user ID from the session
 
-//tell everything was ok
-console.log('Everything executed fine.. website at port 8080....');
+    const paymentData = {
+        userId: userId,
+        amount: parseFloat(amount),
+        cardNumber: cardNumber,
+        cardHolderName: cardHolderName,
+        expirationDate: expirationDate,
+        postalCode: postalCode,
+    };
+
+    try {
+        const payment = new Payment(paymentData);
+        await payment.save();
+        res.render('receipt', { payment: payment });
+    } catch (error) {
+        console.error(error);
+        res.redirect('/payment-failure');
+    }
+});
+
+myApp.get('/payment-success', async (req, res) => {
+    const paymentId = req.query.paymentId; // Assuming you pass the payment ID as a query parameter
+
+    try {
+        const payment = await Payment.findById(paymentId).exec();
+        if (!payment) {
+            console.error('Payment not found'); // Handle not found case
+            res.redirect('/payment-failure');
+        } else {
+            res.render('payment-success', { payment: payment });
+        }
+    } catch (error) {
+        console.error(error);
+        res.redirect('/payment-failure'); // Handle errors appropriately
+    }
+});
 
 
+
+myApp.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
