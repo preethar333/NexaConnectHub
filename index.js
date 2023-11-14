ea91db71bac8074852654e44f9700f93a210da0e
fkskgit <fshaikh0217@conestogac.on.ca>
2023-11-14 12:29:19 -0500

    Md5 Hash(Farheen Shaikh)

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// import dependencies you will use
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -6,10 +5,11 @@ const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const { check, validationResult } = require('express-validator');
 const nameRegex = /^[a-zA-Z0-9]{1,}\s[a-zA-Z0-9]{1,}$/;
+const md5 = require('blueimp-md5');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/NexaConnect');
 
-// Define MongoDB models
 const Blog = mongoose.model('Blog', {
     Name: String,
     Email: String,
@@ -32,23 +32,20 @@ const Payment = mongoose.model('Payment', {
     country: String,
 });
 
-// Configure PayPal
 const paypal = require('paypal-rest-sdk');
 paypal.configure({
-    mode: 'sandbox', // Set to 'sandbox' for testing, 'live' for production
+    mode: 'sandbox',
     client_id: 'AS62O573CXqM8Ww8EEutbpTMHAQ0pv-c6Uy0O5GVFKvkORvRzXmdllNxGUDn2lDYTS2fonmTyjQZtodR',
     client_secret: 'ENNJgDzAapTo6s-pbe1U5Tg4IvUNr6uOLUg9QLinmnzDxwRwAr7gp8_4x4DE4SSICnu2cJ3W8VPmPmvi',
 });
 
 const myApp = express();
-
 myApp.use(express.urlencoded({ extended: false }));
 myApp.use(session({
     secret: 'randomsecretcode',
     resave: false,
     saveUninitialized: true,
 }));
-
 myApp.set('views', path.join(__dirname, 'views'));
 myApp.use(express.static(__dirname + '/public'));
 myApp.set('view engine', 'ejs');
@@ -72,13 +69,13 @@ myApp.get('/signup', (req, res) => {
 myApp.post('/signin', (req, res) => {
     const pageData = {
         username: req.body.userName,
-        password: req.body.password,
+        password: md5(req.body.password),
     };
 
     User.findOne(pageData)
         .then((user) => {
-            req.session.userId = user._id; // Store the user's ID in the session
-            res.render('home');
+            req.session.userId = user._id; 
+            res.redirect('/blogs');
         })
         .catch((err) => {
             console.error(err);
@@ -95,46 +92,14 @@ myApp.post('/createaccount', [
     if (errors.isEmpty()) {
         const pageData = {
             username: req.body.userName,
-            password: req.body.password,
+            password: md5(req.body.password),
         };
 
         const userNew = new User(pageData);
         userNew.save();
-        res.render('login');
-    }
-});
-
-myApp.post('/process', [
-    check('Description', 'Please enter a description.').not().isEmpty(),
-    check('Email', 'Please enter a valid email').isEmail(),
-    check('Name', 'Please enter firstname and lastname').matches(nameRegex),
-], (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        res.render('home', { er: errors.array() });
+        res.redirect('/login');
     } else {
-        const Name = req.body.Name;
-        const Email = req.body.Email;
-        const Description = req.body.Description;
-
-        const Image = req.files.Image;
-        const ImageName = Image.name;
-        const ImageSavePath = filePath + ImageName;
-        Image.mv(ImageSavePath, (err) => {
-            if (err) console.error(err);
-        });
-
-        const pageData = {
-            Name: Name,
-            Email: Email,
-            Description: Description,
-            ImagePath: 'images/' + ImageName,
-        };
-
-        const blogNew = new Blog(pageData);
-        blogNew.save();
-        res.render('blog', pageData);
+        res.render('signup', { errors: errors.array() });
     }
 });
 
@@ -148,6 +113,52 @@ myApp.get('/blogs', (req, res) => {
         });
 });
 
+myApp.get('/blogview/:id', (req, res) => {
+    const blogId = req.params.id;
+    Blog.findById(blogId)
+        .then((blog) => {
+            res.render('blog', { blog });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
+myApp.get('/edit/:id', (req, res) => {
+    const blogId = req.params.id;
+    Blog.findById(blogId)
+        .then((blog) => {
+            res.render('edit', { blog });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
+myApp.post('/edit/:id', (req, res) => {
+    const blogId = req.params.id;
+    const { Name, Email, Description } = req.body;
+
+    Blog.findByIdAndUpdate(blogId, { Name, Email, Description }, { new: true })
+        .then((updatedBlog) => {
+            res.redirect(`/blogview/${updatedBlog._id}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
+myApp.get('/delete/:id', (req, res) => {
+    const blogId = req.params.id;
+    Blog.findByIdAndRemove(blogId)
+        .then(() => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+});
+
 myApp.get('/payment', (req, res) => {
     res.render('payment');
 });
@@ -155,7 +166,6 @@ myApp.get('/payment', (req, res) => {
 myApp.post('/processPayment', (req, res) => {
     const { amount, cardHolderName, email, country, postalCode } = req.body;
 
-    // Store the entered amount and cardHolderName in the session
     req.session.paymentAmount = amount;
     req.session.cardHolderName = cardHolderName;
     req.session.email = email;
@@ -168,13 +178,13 @@ myApp.post('/processPayment', (req, res) => {
             payment_method: 'paypal'
         },
         redirect_urls: {
-            return_url: 'http://localhost:8080/payment-success', // Replace with your success URL
-            cancel_url: 'http://localhost:8080/payment-cancel' // Replace with your cancel URL
+            return_url: 'http://localhost:8080/payment-success',
+            cancel_url: 'http://localhost:8080/payment-cancel'
         },
         transactions: [{
             amount: {
                 total: amount,
-                currency: 'USD' // Change to your preferred currency
+                currency: 'USD'
             },
             description: 'Payment for your order.'
         }]
@@ -183,7 +193,7 @@ myApp.post('/processPayment', (req, res) => {
     paypal.payment.create(createPaymentJson, (error, payment) => {
         if (error) {
             console.error(error);
-            res.redirect('/payment-failure'); // Handle payment creation error
+            res.redirect('/payment-failure');
         } else {
             for (let i = 0; i < payment.links.length; i++) {
                 if (payment.links[i].rel === 'approval_url') {
@@ -198,8 +208,8 @@ myApp.get('/payment-success', (req, res) => {
     const { paymentId, PayerID } = req.query;
     const amount = req.session.paymentAmount;
     const cardHolderName = req.session.cardHolderName;
-    const email = req.session.email; // Retrieve the email from the session
-    const country = req.session.country; // Retrieve the country from the session
+    const email = req.session.email;
+    const country = req.session.country;
     const postalCode = req.session.postalCode;
 
     const executePaymentJson = {
@@ -215,37 +225,31 @@ myApp.get('/payment-success', (req, res) => {
     paypal.payment.execute(paymentId, executePaymentJson, (error, payment) => {
         if (error) {
             console.error(error);
-            res.redirect('/payment-failure'); // Handle payment execution error
+            res.redirect('/payment-failure');
         } else {
-            // Payment was successful, handle accordingly
-            console.log('Payment executed successfully', payment);
-
-            // Save payment details to MongoDB
             const paymentData = new Payment({
-                userId: req.session.userId,  // Assuming you have stored the user's ID in the session
+                userId: req.session.userId,
                 amount: amount,
                 cardHolderName: cardHolderName,
-                email: email, // Save email in the MongoDB document
-                country: country, // Save country in the MongoDB document
+                email: email,
+                country: country,
                 postalCode: postalCode,
             });
 
             paymentData.save()
                 .then((savedPayment) => {
-                    // Handle successful payment and database save here
-                    console.log('Payment details saved to MongoDB:', savedPayment);
                     res.render('payment-success', { amount, cardHolderName, email, country, postalCode });
                 })
                 .catch((dbError) => {
-                    console.error('Error saving payment details to MongoDB:', dbError);
-                    res.redirect('/payment-failure'); // Handle database save error
+                    console.error(dbError);
+                    res.redirect('/payment-failure');
                 });
         }
     });
 });
 
 myApp.get('/payment-cancel', (req, res) => {
-    res.redirect('/payment-failure'); // Handle payment cancellation
+    res.redirect('/payment-failure');
 });
 
 myApp.listen(8080, () => {
